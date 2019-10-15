d6dbd7d07b965a498f68b891dd2890182600ed8b
NikitaPetaichuk <pet.ai.4.uk@yandex.ru>
2019-10-15 19:40:14 +0300

    Comments added

--- Sets_Manager/lingua_docx.py
+++ Sets_Manager/lingua_docx.py
@@ -2,16 +2,20 @@ from docx import Document
 import re
 
 
+# Класс парсера docx файла, хранящего в себе набор слов для серсива Lingua Leo
 class lingua_docx_parser:
+    # Конструктор парсера от пути к файлу
     def __init__(self, file_name):
         self.docx_document = Document(file_name)
 
 
+    # Функция печати содержимого docx файла в консоль
     def print_file(self):
         for paragraph in self.docx_document.paragraphs:
             print(paragraph.text)
 
 
+    # Функция, создающая список слов из docx файла вместе с переводами и транскрипцией
     def create_words_set(self):
         words_set = []
         for paragraph in self.docx_document.paragraphs:
--- Sets_Manager/sets_manager.py
+++ Sets_Manager/sets_manager.py
@@ -3,14 +3,16 @@ import sys
 from lingua_docx import lingua_docx_parser
 
 
+# Функция для создания интерфейса командной строки
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--create', nargs=2)
-    parser.add_argument('-u', '--update', nargs=2)
+    parser.add_argument('-a', '--add', nargs=2)
     parser.add_argument('-r', '--rename', nargs=2)
     return parser
 
 
+# Функция создания набора слов на основе docx файла
 def create_set(set_name, docx_name):
     if docx_name.endswith(".docx"):
         print("Creating set \"{0}\" from file \"{1}\"".format(set_name, docx_name))
@@ -20,7 +22,8 @@ def create_set(set_name, docx_name):
         print("Incorrect file format")
 
 
-def update_set(set_name, docx_name):
+# Функция добавления слов из docx файла в набор слов
+def add_words_to_set(set_name, docx_name):
     if docx_name.endswith(".docx"):
         print("Updating set \"{0}\" using file \"{1}\"".format(set_name, docx_name))
         docx_file = lingua_docx_parser(docx_name)
@@ -29,17 +32,19 @@ def update_set(set_name, docx_name):
         print("Incorrect file format")
 
 
+# Функция переименовывания набора слов
 def rename_set(old_name, new_name):
     print("Renaming set \"{0}\" to \"{1}\"".format(old_name, new_name))
 
 
+# Функция обработки агрументов командной строки
 def start():
     parser = create_parser()
     namespace = parser.parse_args(sys.argv[1:])
     if namespace.create is not None:
         create_set(namespace.create[0], namespace.create[1])
-    if namespace.update is not None:
-        update_set(namespace.update[0], namespace.update[1])
+    if namespace.add is not None:
+        add_words_to_set(namespace.add[0], namespace.add[1])
     if namespace.rename is not None:
         rename_set(namespace.rename[0], namespace.rename[1])
 
